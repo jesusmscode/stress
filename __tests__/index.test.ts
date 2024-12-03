a24665608997e95ce22ab58eea9c76296a2672a8
jesusms <jesusmscod>
2024-12-03 02:39:39 +0100

    chore/ add configuration

---
+++ __tests__/index.test.ts
@@ -0,0 +1,6 @@
+describe('Basic functionality test', () => {
+        it('should add two numbers correctly', () => {
+            const sum = 1 + 2;
+            expect(sum).toBe(3);
+        });
+    });
