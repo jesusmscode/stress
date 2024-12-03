478e49f303f397aed60403c3211edc1aec576d1d
jesusms <jesusmscod>
2024-12-03 18:22:25 +0100

    feat: add basic HTTP server and initial test setup

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,6 +1,24 @@
-describe('Basic functionality test', () => {
-        it('should add two numbers correctly', () => {
-            const sum = 1 + 2;
-            expect(sum).toBe(3);
-        });
+import request from 'supertest';
+import {server} from "../src";
+
+describe('HTTP Server', () => {
+    let app: any;
+
+    beforeAll((done) => {
+        app = server.listen(8080, done);
     });
+
+    afterAll((done) => {
+        app.close(done);
+    });
+
+    it('should bind to port 8080 and respond with 200', async () => {
+        const response = await request(app)
+            .get('/')
+            .expect(200)
+            .expect('Content-Type', /text\/plain/);
+
+        expect(response.text).toBe('Hello, World!');
+    });
+
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import { createServer, Socket } from 'net';
+
+export const server = createServer((socket: Socket) => {
+    socket.on('data', (data) => {
+        const request = data.toString();
+        console.log('Request received:', request);
+
+        const response = [
+            'HTTP/1.1 200 OK',
+            'Content-Type: text/plain',
+            'Content-Length: 13',
+            '',
+            'Hello, World!'
+        ].join('\r\n');
+
+        socket.write(response);
+        socket.end();
+    });
+});
 
